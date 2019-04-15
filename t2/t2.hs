4b3a5f005a3522d169265f6efb67d86cae7cbb3c
fabio <fapedroso@inf.ufsm.br>
2019-04-14 21:51:12 -0300

    SVG 1

--- t2/t2.hs
+++ t2/t2.hs
@@ -1,4 +1,5 @@
 import Text.Printf
+import Data.Fixed
 
 type Point     = (Float,Float)
 type Rect      = (Point,Float,Float)
@@ -9,19 +10,18 @@ type Circle    = (Point,Float)
 -- Paletas
 -------------------------------------------------------------------------------
 
-rgbPalette :: Int -> [(Int,Int,Int)]
-rgbPalette n = take n $ cycle [(255,0,0),(0,255,0),(0,0,255)]
+--rgbPalette :: Int -> [(Int,Int,Int)]
+--rgbPalette n = take n $ cycle [(255,0,0),(0,255,0),(0,0,255)]
 
-greenPalette :: Int -> [(Int,Int,Int)]
-greenPalette n = [(0,80+i*10,0) | i <- [0..n] ]
-
+rgbPalette :: Int -> Int -> [(Int,Int,Int)]
+rgbPalette l c = [if (i `mod` 3) == 0 then (60+(j+(i*c))*5,0,0) else if (i `mod` 3) == 1 then (0,60+(j+(i*c))*5,0) else (0,0,60+(j+(i*c))*5) | i <- [0..l-1], j <- [0..c-1]]
 
 -------------------------------------------------------------------------------
 -- Geração de retângulos em suas posições
 -------------------------------------------------------------------------------
 
-genRectsInLine :: Int -> [Rect]
-genRectsInLine n  = [((m*(w+gap),0.0),w,h) | m <- [0..fromIntegral (n-1)]]
+genRectsInLines :: Int -> Int -> [Rect]
+genRectsInLines l c = [((mx*(w+gap),m*(h+gap)),w,h) | m <- [0..fromIntegral (l-1)] , mx <- [0..fromIntegral (c-1)]]
   where (w,h) = (50,50)
         gap = 10
 
@@ -60,10 +60,12 @@ svgElements func elements styles = concat $ zipWith func elements styles
 
 main :: IO ()
 main = do
-  writeFile "caseX.svg" $ svgstrs
+  writeFile "case1.svg" $ svgstrs
   where svgstrs = svgBegin w h ++ svgfigs ++ svgEnd
         svgfigs = svgElements svgRect rects (map svgStyle palette)
-        rects = genRectsInLine nrects
-        palette = greenPalette nrects
-        nrects = 10
+        rects = genRectsInLines lines columns 
+        palette = rgbPalette lines columns
+        nrects = lines * columns
+        lines = 7
+        columns = 6
         (w,h) = (1500,500) -- width,height da imagem SVG
