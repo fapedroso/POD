c1d0da8f3ea38eea07a4fd27bddfa6717ae5cee6
fapedroso <fabioaraujopedroso@gmail.com>
2019-05-01 17:46:05 -0300

    fixs do t3

--- t3/t3.pl
+++ t3/t3.pl
@@ -6,19 +6,23 @@ odd(N) :-
 tamLis([],0).
 tamLis([H|T],N) :-
   tamLis(T,N1),
-  N is N1 + 1
+  N is N1 + 1,
+  !
   .
 
 hasN(L,N) :-
   tamLis(L,N1),
-  N1 =:= N
+  N1 =:= N,
+  !
   .
+
 inc99([],[]).
 inc99(L1,L2) :-
   L1 = [H|T],
   L3 is H + 99,
   inc99(T,L4),
-  L2 = [L3|L4]
+  L2 = [L3|L4],
+  !
   .
 
 incN([],[],N).
@@ -26,7 +30,8 @@ incN(L1,L2,N) :-
   L1 = [H|T],
   L3 is H + N,
   incN(T,L4,N),
-  L2 = [L3|L4]
+  L2 = [L3|L4],
+  !
   .
 
 comment([],[]).
@@ -34,7 +39,8 @@ comment(L1,L2) :-
   L1 = [H|T],
   string_concat("%%",H,L3),
   comment(T,L4),
-  L2 = [L3|L4]
+  L2 = [L3|L4],
+  !
   .
 
 onlyEven([],[]).
@@ -42,78 +48,94 @@ onlyEven(L1,L2) :-
   L1 = [H|T],
   not(odd(H)),
   onlyEven(T,L3),
-  L2 = L3
+  L2 = L3,
+  !
   .
 onlyEven(L1,L2) :-
   L1 = [H|T],
   odd(H),
   onlyEven(T,L3),
-  L2 = [H|L3]
+  L2 = [H|L3],
+  !
   .
 
 countdown(0,L):-
-  L = []
+  L = [],
+  !
   .
 countdown(N,L) :-
   L = [N|T],
   Ns is N-1,
-  countdown(Ns,T)
+  countdown(Ns,T),
+  !
   .
 
 nRandoms(0,L) :-
-  L = []
+  L = [],
+  !
   .
 nRandoms(N,L) :-
   random_between(0, 99, H),
   L = [H|T],
   Ns is N-1,
-  nRandoms(Ns,T)
+  nRandoms(Ns,T),
+  !
   .
 
-pot(N,0,1).
+pot(N,0,1) :- 
+  !
+  .
 pot(N,M,X) :-
   Ms is M-1,
   pot(N,Ms,Xs),
-  X is N * Xs
+  X is N * Xs,
+  !
   .
 
 potN0(0,L) :-
   pot(2,0,H),
-  L = [H]
+  L = [H],
+  !
   .
 potN0(N,L) :-
   pot(2,N,H),
   L = [H|T],
   Ns is N-1,
-  potN0(Ns,T)
+  potN0(Ns,T),
+  !
   .
 
 zipmult([],[],L3) :-
-  L3 = []
+  L3 = [],
+  !
   .
 zipmult(L1,[],L3) :-
   L1 = [H1|T1],
   zipmult(T1,[],T),
   H is H1*1,
-  L3 = [H|T]
+  L3 = [H|T],
+  !
   .
 zipmult([],L2,L3) :-
   L2 = [H2|T2],
   zipmult([],T2,T),
   H is 1*H2,
-  L3 = [H|T]
+  L3 = [H|T],
+  !
   .
 zipmult(L1,L2,L3) :-
   L1 = [H1|T1],
   L2 = [H2|T2],
   zipmult(T1,T2,T),
   H is H1*H2,
-  L3 = [H|T]
+  L3 = [H|T],
+  !
   .
 
 pots_aux(Ni,Nf,L) :-
   Ni > Nf,
-  L = []
+  L = [],
+  !
   .
 pots_aux(Ni,Nf,L) :-
   Ni =< Nf,
@@ -121,29 +143,35 @@ pots_aux(Ni,Nf,L) :-
   pot(2,Nis,H),
   M is Ni+1,
   pots_aux(M,Nf,T),
-  L = [H|T]
+  L = [H|T],
+  !
   .
 
 potencias(0,L) :-
-  L = []
+  L = [],
+  !
   .
 potencias(N,L) :- 
-  pots_aux(1,N,L)
+  pots_aux(1,N,L),
+  !
   .
 
 recursiveDivision(Divisor,Divident,Quotient) :-
   Divisor > Divident,
-  Quotient = 0
+  Quotient = 0,
+  !
   .
 recursiveDivision(Divisor,Divident,Quotient) :-
   Divisor =< Divident,
   DividentS is Divident - Divisor,
   recursiveDivision(Divisor,DividentS,QuotientS),
-  Quotient is QuotientS + 1
+  Quotient is QuotientS + 1,
+  !
   .
   
 cedulas(V,[],L2) :-
-  L2 = []
+  L2 = [],
+  !
   .
 cedulas(V,L1,L2) :- 
   L1 = [H1|T1],
@@ -151,6 +179,7 @@ cedulas(V,L1,L2) :-
   recursiveDivision(H1,V,Q),
   H2 is Q,
   R is V-H1*Q, 
-  cedulas(R,T1,T2)
+  cedulas(R,T1,T2),
+  !
   .
   
